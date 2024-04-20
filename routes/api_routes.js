95577dc25b49a66a478fbaf8092036693d1319bb
Porsche <herskorn@gmail.com>
2024-04-20 12:02:42 -0700

    added delete request function to api_routes.js

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -24,7 +24,25 @@ router.post('/api/notes', (req, res) => {
     fs.writeFileSync('db/db.json', JSON.stringify(jsonDatabase));
     //Responds with Updated Data
     res.json(jsonDatabase);
+});
+
+//Remove Note Object From db.json//
+//Delete Request Via JSON Object's ID Parameter
+router.delete('/api/notes/:id', (req, res) => {
+    //JSON Data is Read and Parsed
+    let jsonData = fs.readFileSync('db/db.json', 'utf8');
+    const note = JSON.parse(jsonData);
+    /*Filter Objects by ID Parameter and Return an Array with Only Objects Whose ID 
+    Does Not Match the Request Object*/
+    const deleteNote = note.filter((note) => {
+        return note.id !== req.params.id;
+    });
+
+    //Write New Array to JSON file
+    fs.writeFileSync('db/db.json', JSON.stringify(deleteNote));
+    res.json('Note has been deleted.')
 })
+
 
 //Export Allowing the Rest of the App to Access the Router Object
 module.exports = router;
